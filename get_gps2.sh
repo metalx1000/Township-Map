f3e122f9a95a0c40e9dff3148aca21950ae7bc5c
metalx1000 <github@filmsbykris.com>
2015-03-28 14:03:30 -0400

    shell script to grid townships

---
+++ get_gps2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+nwX=26.1962553
+nwY=-81.8161969
+X=0.0143900
+Y=0.0160968
+
+for l in `seq 0 10`
+do
+  for i in `seq 0 10`
+  do
+    z=$(echo "$i + 1"|bc -l)
+    a=$(echo "$l + 1"|bc -l)
+    lat1="$(echo "$nwX - $X * $l"|bc -l)"
+    long1="$(echo "$nwY + $Y * $i"|bc -l)"
+    lat2="$(echo "$nwX - $X * $a"|bc -l)"
+    long2="$(echo "$nwY + $Y * $z"|bc -l)"
+
+    echo "make_box($lat1,$long1,$lat2,$long2)"
+  done
+done
